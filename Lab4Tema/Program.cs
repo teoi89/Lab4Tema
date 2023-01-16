9f78af9e67e2d0b97d0dc6335a2d986920d6b5fe
teoi89 <teofil.lucaci@yahoo.ro>
2023-01-16 22:32:46 +0200

    Exercitiile 1, 2, 3, 5

--- Lab4Tema/Program.cs
+++ Lab4Tema/Program.cs
@@ -7,7 +7,9 @@ namespace Lab4Tema
         static void Main(string[] args)
         {
             //Ex1();
-            Ex2();
+            //Ex2();
+            //Ex3();
+            Ex5();
         }
 
         static void Ex1()
@@ -25,9 +27,24 @@ namespace Lab4Tema
                 rezultatul.
              * Scrieti o functie care va determina elementul cu valoare maxima, apelati-o si afisati-i
                 rezultatul. */
-            CalculeazaSuma(ReadMatrice());
-            ElementMaxim(ReadMatrice());
+            CalculeazaSuma(ReadMatriceWithThreeDimensions());
+            ElementMaxim(ReadMatriceWithThreeDimensions());
         }
+        static void Ex3()
+        {
+            /* Cititi de la tastatura continutul a doua matrici de intregi cu 2 dimensiuni avand lungimile n
+            m, respectiv m, n. Lungimile celor doua dimensiuni ale matricilor, m si n, vor fi citite de la
+            tastaura.Scrieti o functie care va calcula produsul celor doua matrici, apelati-o si afisati-I
+            rezultatul. */
+            ReadMatriceWithTwoDimensions();
+        }
+        static void Ex5()
+        {
+            /*Scrieti o functie recursiva care va calcula suma numerelor de la 1 pana la n,
+            apelati-o si afisati-i rezultatul.  */
+            Console.WriteLine(ReturnSum(ReadNumber()));
+        }
+
         static int[] ReadVector()
         {
             Console.WriteLine("Introduceti dimensiunea vectorului:");
@@ -60,7 +77,7 @@ namespace Lab4Tema
             }
             return vector;
         }
-        static int[,,] ReadMatrice()
+        static int[,,] ReadMatriceWithThreeDimensions()
         {
             Console.WriteLine("Introduceti dimensiunile matricii (n m k):");
             Console.WriteLine("Introduceti dimensiunea matricii n");
@@ -101,6 +118,7 @@ namespace Lab4Tema
                     }
                 }
             }
+            Console.WriteLine("Suma elementelor matricii este: " + suma);
             return suma;
         }
         static int ElementMaxim(int[,,] matrice)
@@ -119,7 +137,82 @@ namespace Lab4Tema
                     }
                 }
             }
+            Console.WriteLine("Elementul maxim al matricii este: " + max);
             return max;
+        }
+        static int[,] ReadMatriceWithTwoDimensions()
+        {
+            Console.WriteLine("Enter the dimensions of the first array (n m):");
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int[,] array1 = new int[n, m];
+
+            Console.WriteLine("Enter the elements of the first array:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    array1[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("Enter the dimensions of the second array (m n):");
+            int m2 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int[,] array2 = new int[m2, n2];
+
+            Console.WriteLine("Enter the elements of the second array:");
+            for (int i = 0; i < m2; i++)
+            {
+                for (int j = 0; j < n2; j++)
+                {
+                    array2[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            return CalculateProduct(array1, array2);
+        }
+        static int[,] CalculateProduct(int[,] array1, int[,] array2)
+        {
+            int n = array1.GetLength(0);
+            int m = array1.GetLength(1);
+            int n2 = array2.GetLength(1);
+            int[,] product = new int[n, n2];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n2; j++)
+                {
+                    int sum = 0;
+                    for (int k = 0; k < m; k++)
+                    {
+                        sum += array1[i, k] * array2[k, j];
+                    }
+                    product[i, j] = sum;
+                }
+            }
+            Console.WriteLine("The product of the two arrays is:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n2; j++)
+                {
+                    Console.Write(product[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            return product;
+        }
+        static int ReadNumber() 
+        {
+            Console.WriteLine("Introduceti un numar");
+            int n = int.Parse(Console.ReadLine());
+            return n;
+        }
+        static int ReturnSum(int n)
+        {
+            if (n == 1)
+                return 1;
+            else
+                return n + ReturnSum(n - 1);
+
         }
     }
 }
